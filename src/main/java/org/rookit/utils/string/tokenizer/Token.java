481af3574bf43e54979199daa8cc2f7385486ae4
João Sousa <jpd.sousa@campus.fct.unl.pt>
2018-08-13 20:09:08 +0100

    Null Object implementations for common IO types

--- src/main/java/org/rookit/utils/string/tokenizer/Token.java
+++ src/main/java/org/rookit/utils/string/tokenizer/Token.java
@@ -3,6 +3,7 @@ package org.rookit.utils.string.tokenizer;
 import org.immutables.value.Value;
 
 @Value.Immutable
+@Value.Style(canBuild = "isBuildable")
 public interface Token {
 
     String token();
