3f40ec8f54c7f4254a43b0d2e369bf8fbfd6a54a
João Sousa <jpd.sousa@campus.fct.unl.pt>
2019-08-31 09:25:55 +0100

    String module for lib module

---
+++ lib/src/main/java/org/rookit/utils/string/StringModule.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (C) 2018 Joao Sousa
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+package org.rookit.utils.string;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Module;
+import com.google.inject.Singleton;
+import com.google.inject.util.Modules;
+import org.rookit.utils.string.join.JointStringModule;
+import org.rookit.utils.string.template.TemplateModule;
+
+public final class StringModule extends AbstractModule {
+
+    private static final Module MODULE = Modules.combine(
+            new StringModule(),
+            JointStringModule.getModule(),
+            TemplateModule.getModule()
+    );
+
+    public static Module getModule() {
+        return MODULE;
+    }
+
+    private StringModule() {}
+
+    @Override
+    protected void configure() {
+        bind(StringUtils.class).to(StringUtilsImpl.class).in(Singleton.class);
+    }
+}
